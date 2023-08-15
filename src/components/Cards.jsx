29bc5a0dc889131384ecd763e787312a17e3ab78
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-08-15 19:37:28 +0530

    added parent platforms to the cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,59 +1,71 @@
 "use client";
 import useAxios from "@/hooks/useAxios";
 import Card from "./Card";
-import {BsPlaystation, BsWindows} from "react-icons/bs"
+
+import { platforms } from "@/lib/utils";
 export default function Cards() {
   const { response } = useAxios(`/games?key=${process.env.KEY}`);
-  if(!response){
-      return (
-        <div className="w-full h-screen flex items-center justify-center"> <div class="lds-ripple"><div></div><div></div></div></div>
-      )
+  if (!response) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        {" "}
+        <div className="lds-ripple">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
   }
   return (
     <>
-    <h1 className=" text-black text-center font-bold text-3xl mt-12">All Games</h1>
+      <h1 className=" text-black dark:text-white text-center font-bold text-3xl mt-12">
+        All Games
+      </h1>
       <div className="justify-center pt-8 flex gap-8 flex-wrap">
-        
-        {
-            response.results.map((items) => {
-                return (
-                    <div key={items.id} class="w-96 bg-white border border-gray-200 rounded-xl shadow-xl dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                      <img
-                        class="rounded-t-xl w-full h-52 object-cover"
-                        src={items.background_image}
-                        alt=""
-                      />
-                    </a>
-                    <div class="p-5 flex justify-between items-end">
-                  <div>
+        {response.results.map((items) => {
+          console.log(items.parent_platforms);
+
+          return (
+            <div
+              key={items.id}
+              className="w-96 bg-white border border-gray-200 rounded-xl shadow-xl dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <img
+                  className="rounded-t-xl w-full h-52 object-cover"
+                  src={items.background_image}
+                  alt=""
+                />
+              </a>
+              <div className="p-5 flex justify-between items-end">
+                <div>
                   <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                          {items.name}
-                        </h5>
-                      </a>
-                      <p className="flex gap-2 pb-1">
-                      <BsPlaystation/>
-                      <BsWindows/>
-                      </p>
-                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        {items.released}
-                      </p>
-                  </div>
-                      <div>
-                      <a
-                        href="#"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                        {items.metacritic}
-          
-                      </a>
-                      </div>
-                    </div>
-                  </div>
-                )
-            })
-        }
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {items.name}
+                    </h5>
+                  </a>
+                  <p className="flex gap-2 pb-1">
+                    {items.parent_platforms.map((item) => {
+                      const name = item?.platform.slug;
+                      return platforms(name);
+                    })}
+                  </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {items.released}
+                  </p>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {items.metacritic}
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,30 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { BsPlaystation, BsWindows, BsXbox, BsNintendoSwitch, BsApple, BsAndroid } from "react-icons/bs";
+import {DiLinux} from "react-icons/di";
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
+}
+export function platforms(name) {
+  if (name === "pc") {
+    return <BsWindows />;
+  }
+  if (name === "playstation") {
+    return <BsPlaystation />;
+  }
+  if (name === "xbox") {
+    return <BsXbox />;
+  }
+  if(name === "nintendo"){
+    return<BsNintendoSwitch/>;
+  }
+  if(name === "mac"){
+    return <BsApple/>
+  }
+  if(name === "linux") {
+    return  <DiLinux />
+  }
+  if(name === "android"){
+    return   <BsAndroid/>
+  }
 }
